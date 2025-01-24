b4b734caff87e10842919bde9745a8e161bfee3e
ChaudhryYahya <yahya.chaudhry@student.howest.be>
2025-01-24 19:00:55 +0500

    final changes to summarizing hyperparameters

--- app/summarizer.py
+++ app/summarizer.py
@@ -3,7 +3,7 @@ from transformers import pipeline
 # Initialize the summarizer (only once)
 summarizer_pipeline = pipeline("summarization", model="t5-small")
 
-def summarize_text(text, max_length=500, min_length=50):
+def summarize_text(text, max_length=150, min_length=50):
     summary_outputs = summarizer_pipeline(
         text, 
         max_length=max_length, 
